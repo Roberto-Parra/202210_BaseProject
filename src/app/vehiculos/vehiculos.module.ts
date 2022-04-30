7117135b3feacc98b09c671d4ca668cec5194f0a
Roberto-Parra <w.parrav@uniandes.edu.co>
2022-04-30 00:45:54 -0400

    Se exporta componente en el modulo

--- src/app/vehiculos/vehiculos.module.ts
+++ src/app/vehiculos/vehiculos.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { VehiculosListComponent } from './vehiculos-list/vehiculos-list.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule],
+  exports: [VehiculosListComponent],
+  declarations: [VehiculosListComponent]
 })
 export class VehiculosModule { }
