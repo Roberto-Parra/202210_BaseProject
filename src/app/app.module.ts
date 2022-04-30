43fd3c62321a2a91097b58993db355f406cbd580
Roberto-Parra <w.parrav@uniandes.edu.co>
2022-04-30 08:58:21 -0400

    Se actualiza titulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VehiculosModule } from './vehiculos/vehiculos.module'
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { VehiculosModule } from './vehiculos/vehiculos.module'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    VehiculosModule
+    VehiculosModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
